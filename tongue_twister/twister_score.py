68a09b25ad343d73a9a244953284e78383824db3
pmuruga <pmurugan@mit.edu>
2019-09-14 21:25:27 -0400

    update scoring

--- tongue_twister/twister_score.py
+++ tongue_twister/twister_score.py
@@ -1,6 +1,8 @@
 import pyphen
 import re
 import math
+from collections import Counter
+import numpy as np
 
 def score(text):
     dic = pyphen.Pyphen(lang='en')
@@ -16,23 +18,38 @@ def score(text):
             s = homophones2[s[:2]] + s[2:]
         if s[:1] in homophones1:
             s = homophones1[s[:1]] + s[1:]
-
+    
+    exempt_list = ['a','an','the','of','by','to','at','on','be',"i",'and']
+    
     initials = {i: 0 for i in "abcdefghijlmnopqrstuvwyz"}
     for s in syllables:
         initials[s[0]] += 1
-
+    #print(syllables)
+    start = [s[0] for s in syllables]
+    let = Counter(start)
     bonus = 0
     n = 0
-    syllables.append('?') # to make sure the last factorial is computed
-    for i in range(len(syllables)-1):
-        word1 = syllables[i]
-        word2 = syllables[i+1]
+    syl_significant = []
+    for i in range(len(syllables)):
+        try:
+            if not (syllables[i] in exempt_list and syllables[i+1] not in exempt_list):
+                syl_significant.append(syllables[i])
+        except:
+            pass
+    #print(syl_significant)
+    syl_significant.append('?') # to make sure the last factorial is computed
+    for i in range(len(syl_significant)-1):
+        word1 = syl_significant[i]
+        word2 = syl_significant[i+1]
         if word2[0] == word1[0]:
             n += 1
         else:
-            bonus += 2**n
+            if n > 4:
+                bonus += 2**((n+4)/2)
+            else:
+                bonus += 2**n
             n = 0
+    #print(bonus)
+    return int(bonus + np.mean([let[l]**2 for l in let])/np.log(len(syl_significant)+10)*np.log(10))
 
-    return bonus + sum([2*initials[i] for i in initials])
-
-print(score(input("your tongue twister: ")))
+#print(score(input("your tongue twister: ")))
