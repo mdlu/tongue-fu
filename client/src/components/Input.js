ed3e202dba612cfb370f1be914ccda3e6f75c6ab
Christian Scarlett <christianscarlett21@gmail.com>
2019-09-14 21:58:07 -0400

    added rudimentary record button
    Still needed:
    global permission tracking
    what to do with audio after it is recorded

--- client/src/components/Input.js
+++ client/src/components/Input.js
@@ -4,35 +4,71 @@ class Input extends React.Component {
 
   constructor (props) {
       super(props);
-      this.state = {
-        value: "",
-        numInputs: 0,
-        inputsArray: []
-      };
-      this.handleChange = this.handleChange.bind(this);
-      this.handleSubmit = this.handleSubmit.bind(this);
+      // this.state = {
+      //   value: "",
+      //   numInputs: 0,
+      //   inputsArray: []
+      // };
+      // this.handleChange = this.handleChange.bind(this);
+      // this.handleSubmit = this.handleSubmit.bind(this);
+
   }
 
-  handleChange(event) {
-    this.setState({value: event.target.value});
-    console.log(this.state.value);
+  record(){
+
+    const recordButton = document.getElementById("record");
+    let stream = null;
+
+    navigator.mediaDevices.getUserMedia({ audio: true })
+    .then(stre => {
+        this.stream = stre;
+    });
+
+
+    const mediaRecorder = new MediaRecorder(stream);
+    mediaRecorder.start();
+
+    const audioChunks = [];
+
+    mediaRecorder.addEventListener("dataavailable", event => {
+        audioChunks.push(event.data);
+    });
+
+    mediaRecorder.addEventListener("stop", () => {
+        const audioBlob = new Blob(audioChunks, {type: 'audio/webm'});
+        // Send audio blob to be processed
+
+        // const audioUrl = URL.createObjectURL(audioBlob);
+        // const down = document.getElementById("download");
+        // down.href = audioUrl;
+        // down.download = "test_audio.webm";
+    });
+
+    setTimeout(() => {
+    mediaRecorder.stop();
+    }, 3000);
+
   }
 
-  handleSubmit(event) {
-    event.preventDefault();
-    this.props.onSubmit(this.state.value);
-    this.setState({
-      value: ""
-    })
-  }
+  // handleChange(event) {
+  //   this.setState({value: event.target.value});
+  //   console.log(this.state.value);
+  // }
+
+  // handleSubmit(event) {
+  //   event.preventDefault();
+  //   this.props.onSubmit(this.state.value);
+  //   this.setState({
+  //     value: ""
+  //   })
+  // }
+
+
 
   render() {
     return (
       <div className="promptInput">
-      <form onSubmit={this.handleSubmit}>
-        <textarea className="form-control form-control-lg input-text" type="text" placeholder="Type here..." value={this.state.value} onChange={this.handleChange} rows="3"/>
-        <input type="submit" value="" className="btn btn-light floating-button send-button"disabled={!this.state.value} />
-      </form>
+        <button id="record" onClick={this.record}>Record Audio</button>
       </div>
     )
     ;
